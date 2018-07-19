6d9ec45a6f2b41b40c6cc5c260bb751be1a8f1e4
GeorgeNet <g@bougakov.com>
2018-07-19 12:18:57 +0300

    add contrib links
    Signed-off-by: GeorgeNet <g@bougakov.com>

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,16 @@ module.exports = {
   dest: 'docs',
   title: 'Jebase',
   themeConfig: {
-    sidebar: false
+    sidebar: false,
+    repo: 'gbougakov/jebase',
+    // Optional options for generating "Edit this page" link
+
+    // if your docs are in a different repo from your main project:
+    docsRepo: 'gbougakov/jebase',
+    // if your docs are in a specific branch (defaults to 'master'):
+    docsBranch: 'master',
+    // defaults to false, set to true to enable
+    editLinks: true
   },
   base: '/jebase/'
 }
